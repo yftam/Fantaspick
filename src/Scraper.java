ad765c838c0ddb87dd70aa5b4cba1d56d0cda424
yftam <anthony.tam.junk@live.com>
2018-01-03 11:02:16 -0700

    20180103
    20180103

---
+++ src/Scraper.java
@@ -0,0 +1,59 @@
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+public class Scraper {
+	File file;
+	PrintWriter printwrite;
+
+	
+	
+	public void createFile(String dirName, String fileName) throws IOException{
+		file = new File(dirName + "/" +fileName);
+		file.getParentFile().mkdirs();
+		file.createNewFile();			
+	}
+
+	public void openFile(String fileName) throws IOException {
+		printwrite = new PrintWriter(new FileOutputStream (new File(fileName),true));
+		
+	}
+
+
+	public void closeFile() throws IOException {
+		printwrite.close();
+	}
+
+
+	public void startScrape(String url) throws Exception {
+
+		final Document document = Jsoup.connect(url).get();		
+		Elements elements = document.select("div#zg_centerListWrapper");		
+		for(Element element : elements.select("div.zg_itemImmersion")) {				
+			for(Element subelement1 : element.select("div.zg_itemWrapper")) {
+				String itemImgURL = subelement1.select("div.a-section img").attr("src");// image
+				String itemName = subelement1.select("div.p13n-sc-truncate").text();// name
+				String itemRating = subelement1.select("div.a-icon-row a").attr("title");// rating		
+				String itemPrice = subelement1.select("div.a-row span.p13n-sc-price").text();// price
+				String itemReviewTimes = subelement1.select("div.a-icon-row a.a-size-small").text();// Number of Reviews
+				String itemPrime = subelement1.select("div.a-row i").attr("aria-label");// Prime?
+				printwrite.println(new Date().toString()+";"+itemName+";"+itemRating+";"+itemPrice+";"+itemReviewTimes+";"+itemPrime);
+				System.out.println(new Date().toString()+";"+itemName+";"+itemRating+";"+itemPrice+";"+itemReviewTimes+";"+itemPrime);
+
+			}
+		}
+	}
+
+
+
+}// end class
